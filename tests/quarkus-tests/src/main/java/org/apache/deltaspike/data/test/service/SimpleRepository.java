89c79f3b20e8550868f49d3fd9dc44a7c73d1da4
Mart Somermaa <mrts@users.noreply.github.com>
2025-06-03 22:44:53 +0300

    WIP

---
+++ tests/quarkus-tests/src/main/java/org/apache/deltaspike/data/test/service/SimpleRepository.java
@@ -0,0 +1,100 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements. See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership. The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+package org.apache.deltaspike.data.test.service;
+
+import ee.hiberspike.data.EntityRepository;
+import org.apache.deltaspike.data.test.domain.Simple;
+import org.hibernate.annotations.processing.Find;
+import org.hibernate.annotations.processing.HQL;
+import org.hibernate.annotations.processing.SQL;
+import org.hibernate.query.Page;
+
+import java.util.List;
+
+public interface SimpleRepository extends EntityRepository<Simple, Long> {
+
+    @HQL("select s from Simple s where s.name = ?1 and s.enabled = ?2 order by s.id asc")
+    List<Simple> findByNamedQueryIndexed(String name, Boolean enabled);
+
+    @HQL("select e from Simple e where e.name like ?1")
+    Simple findByNameOptional(String name);
+
+    @HQL("select e from Simple e where e.name like ?1")
+    Simple findByNameAny(String name);
+
+    @HQL("select s from Simple s where s.name = ?1 and s.enabled = ?2 order by s.id asc")
+    List<Simple> findByNamedQueryRestricted(String name, Boolean enabled, Page page);
+
+    // TODO: lock = PESSIMISTIC_WRITE
+    @HQL("select s from Simple s where s.id = :id and s.enabled = :enabled")
+    Simple findByNamedQueryNamed(Long id, Boolean enabled);
+
+    @HQL("select s from Simple s where s.name = ?1")
+    Simple findByQuery(String name);
+
+    @HQL("select count(s) from Simple s where s.name = ?1")
+    Long findCountByQuery(String name);
+
+    // FIXME: naming convention based queries, non-trivial conversion
+    @Find
+    Simple findByNameAndEnabled(String name, Boolean enabled);
+
+    @Find
+    Simple findByNameLikeIgnoreCase(String name);
+
+    @Find
+    Simple findByNameIgnoreCase(String name);
+
+    @Find
+    Simple findOptionalByName(String name);
+
+    @Find
+    Simple findAnyByName(String name);
+
+    @Find
+    List<Simple> findFirst2ByName(String name);
+
+    @Find
+    List<Simple> findTop2ByName(String name);
+
+    @Find
+    List<Simple> findFirst3OrderByName();
+
+    @Find
+    List<Simple> findAllOrderByName();
+
+    long countByName(String name);
+
+    List<Simple> findByOrderByCounterAscIdDesc();
+
+    @SQL(value = "SELECT * from SIMPLE_TABLE s WHERE s.name = ?1")
+    List<Simple> findWithNative(String name);
+
+    @HQL("update Simple as s set s.name = ?1 where s.id = ?2")
+    int updateNameForId(String name, Long id);
+
+    void deleteByName(String name);
+
+    void removeByName(String name);
+
+    void deleteByNameAndEnabled(String name, boolean enable);
+
+    void removeByNameAndEnabled(String name, Boolean aTrue);
+
+}
