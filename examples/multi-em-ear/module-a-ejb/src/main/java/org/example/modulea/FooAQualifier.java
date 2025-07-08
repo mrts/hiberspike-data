ad88e46fecf2d7b4379a26f2bc5077d669bb6fb1
Mart Somermaa <mrts@users.noreply.github.com>
2025-07-08 17:24:41 +0300

    examples/multi-em-ear: use the @Target({ElementType.TYPE_USE qualifier annotation hack with patched jpamodelgen to get EntityManager working with qualifier annotations
    See https://github.com/mrts/hiberspike-data/wiki/jpamodelgen-error-report why this is needed.
    See https://github.com/mrts/hiberspike-data/wiki/jpamodelgen-error-report for jpamodelgen patching instructions.

--- examples/multi-em-ear/module-a-ejb/src/main/java/org/example/modulea/FooAQualifier.java
+++ examples/multi-em-ear/module-a-ejb/src/main/java/org/example/modulea/FooAQualifier.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
+@Target({ElementType.TYPE_USE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Qualifier
 public @interface FooAQualifier {
--- examples/multi-em-ear/module-b-ejb/src/main/java/org/example/moduleb/FooBQualifier.java
+++ examples/multi-em-ear/module-b-ejb/src/main/java/org/example/moduleb/FooBQualifier.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.FIELD, ElementType.METHOD, ElementType.PARAMETER})
+@Target({ElementType.TYPE_USE, ElementType.METHOD, ElementType.FIELD, ElementType.PARAMETER})
 @Retention(RetentionPolicy.RUNTIME)
 @Qualifier
 public @interface FooBQualifier {
